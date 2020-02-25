f12d2ec10ad0e8be85b84c6d8b207dfc2574fd5e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-25 14:21:57 +0000

    build based on 99e8062

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SeqData-1","page":"Home","title":"SeqData","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"SeqData is a small package that provides a data type for sequential data for learning problems. It saves a lot of memory as it does not save the identical data many times but uses a modified indexing.","category":"page"},{"location":"#Usage-1","page":"Home","title":"Usage","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Everything you need to now is in the doc string:","category":"page"},{"location":"#","page":"Home","title":"Home","text":"AbstractSeqData\nSeqData","category":"page"}]
+}
